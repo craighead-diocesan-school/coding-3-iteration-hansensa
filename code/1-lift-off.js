5628567cde83138d58ca0daa2e637a322588472f
hansensa <hansensa48@craighead.school.nz>
2025-06-09 13:29:33 +1200

    Update 1-lift-off.js

---
+++ code/1-lift-off.js
@@ -0,0 +1,26 @@
+// ####################################
+// ####### ----- Lift Off ----- #######
+// ####################################
+
+function liftOff() {
+
+  let count = prompt('Pick a number to count down from.')
+  
+  while (count > 0) {
+    alert(count + '...')
+    count = count - 1
+  }
+   
+  alert('Lift Off!')
+}
+
+function make1() {
+   let count = prompt('Pick a number to count to.')
+  let countUp = prompt('Pick a number to count up by.') 
+  while(countUp <= count){
+  alert(count + '...')
+  count = countUp + countUp
+}
+  }
+  alert('Lift Off!')
+// copy and paste your code here when you get to the Make step
