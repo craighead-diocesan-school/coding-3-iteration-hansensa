b0ea9670eb634b392ac10ab682398caa3f7cfb28
hansensa <hansensa48@craighead.school.nz>
2025-06-17 14:28:01 +1200

    Update 3-square-series.js

--- code/3-square-series.js
+++ code/3-square-series.js
@@ -3,5 +3,16 @@
 // ####################################
 
 function squareSeries() {
+  let count = 0
+  let squareSeries = prompt('What number do you want the Square Series for?')
+  squareSeries = Number(squareSeries)
+  let series = ''
+  while (count < squareSeries){
+    number = squareSeries * squareSeries
+    series = series + number + ', '
+    count = count + 1
+    squareSeries = squareSeries - 1
+  }
+  alert('The series is ' + series + '.')
   // write your code here
 }
