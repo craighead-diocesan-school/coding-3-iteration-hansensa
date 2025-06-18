fac735d41072b8e9b36a025f3dd994ed156ad059
hansensa <hansensa48@craighead.school.nz>
2025-06-19 08:48:54 +1200

    square

--- code/3-square-series.js
+++ code/3-square-series.js
@@ -7,11 +7,12 @@ function squareSeries() {
   let squareSeries = prompt('What number do you want the Square Series for?')
   squareSeries = Number(squareSeries)
   let series = ''
+  let square = squareSeries
   while (count < squareSeries){
-    number = squareSeries * squareSeries
+    let number = square * square
     series = series + number + ', '
     count = count + 1
-    squareSeries = squareSeries - 1
+    square = square - 1
   }
   alert('The series is ' + series + '.')
   // write your code here
--- code/4-test-factorial.js
+++ code/4-test-factorial.js
@@ -3,5 +3,17 @@
 // ####################################
 
 function factorial() {
+  let count = 0
+  let factor = prompt('What number do you want that Factorial for?')
+  factor = Number(factor)
+  series = ''
+  while(count<factor){
+    let number = factor * factor
+    number = Number(number)
+    series = series + number
+    count = count + 1
+    factor = factor
+  }
+  alert('The factorial is ' + series)
   // write your code here
 }
